4a8886731db54789f49acd523147c974565343d0
Ka Wa Ho <ka.wa.ho@cern.ch>
2025-04-10 19:43:00 +0200

    add MLPF PU prediction

--- DataFormats/ParticleFlowCandidate/src/PFCandidate.cc
+++ DataFormats/ParticleFlowCandidate/src/PFCandidate.cc
@@ -46,6 +46,7 @@ PFCandidate::PFCandidate()
       dnn_e_bkgTau_(PFCandidate::bigMva_),
       dnn_e_bkgPhoton_(PFCandidate::bigMva_),
       dnn_gamma_(PFCandidate::bigMva_),
+      mlpf_pu_(PFCandidate::bigMva_),
       getter_(nullptr),
       storedRefsBitPattern_(0),
       time_(0.f),
@@ -91,6 +92,7 @@ PFCandidate::PFCandidate(Charge charge, const LorentzVector& p4, ParticleType pa
       dnn_e_bkgTau_(PFCandidate::bigMva_),
       dnn_e_bkgPhoton_(PFCandidate::bigMva_),
       dnn_gamma_(PFCandidate::bigMva_),
+      mlpf_pu_(PFCandidate::bigMva_),
       getter_(nullptr),
       storedRefsBitPattern_(0),
       time_(0.f),
@@ -153,6 +155,7 @@ PFCandidate::PFCandidate(PFCandidate const& iOther)
       dnn_e_bkgTau_(iOther.dnn_e_bkgTau_),
       dnn_e_bkgPhoton_(iOther.dnn_e_bkgPhoton_),
       dnn_gamma_(iOther.dnn_gamma_),
+      mlpf_pu_(iOther.mlpf_pu_),
       positionAtECALEntrance_(iOther.positionAtECALEntrance_),
       getter_(iOther.getter_),
       storedRefsBitPattern_(iOther.storedRefsBitPattern_),
@@ -203,6 +206,7 @@ PFCandidate& PFCandidate::operator=(PFCandidate const& iOther) {
   dnn_e_bkgTau_ = iOther.dnn_e_bkgTau_;
   dnn_e_bkgPhoton_ = iOther.dnn_e_bkgPhoton_;
   dnn_gamma_ = iOther.dnn_gamma_;
+  mlpf_pu_ = iOther.mlpf_pu_;
   positionAtECALEntrance_ = iOther.positionAtECALEntrance_;
   getter_ = iOther.getter_;
   storedRefsBitPattern_ = iOther.storedRefsBitPattern_;
