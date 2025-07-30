f39bc8e38a86afadf0ce3e3f8dcb1a81e7a3875e
Joosep Pata <joosep.pata@cern.ch>
2025-07-30 17:19:00 +0300

    PU cut at 0.4

--- CommonTools/PileupAlgos/python/Puppi_cff.py
+++ CommonTools/PileupAlgos/python/Puppi_cff.py
@@ -84,7 +84,7 @@ mlpfpu = MLPFPUProducer.clone(
                       UseFromPV2Recovery = True,
                       PtMinForFromPV2Recovery = 4.,
                       clonePackedCands   = False, # should only be set to True for MiniAOD
-                      mlpfPUCut = 0.1,
+                      mlpfPUCut = 0.4,
                       applyCHS = True,
                       applyMLPF = False
 )
