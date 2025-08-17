ead6d2ca7041a2cbde98c63ef3ec65ccbb87cde2
Joosep Pata <joosep.pata@cern.ch>
2025-08-17 20:51:19 +0300

    update model to 20250809

--- RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
+++ RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
@@ -268,7 +268,7 @@ void MLPFProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions
   desc.add<edm::InputTag>("src", edm::InputTag("particleFlowBlock"));
   desc.add<edm::FileInPath>("model_path",
                             edm::FileInPath("RecoParticleFlow/PFProducer/data/mlpf/"
-                                            "mlpf_5M_attn2x3x256_bm12_relu_checkpoint10_8xmi250_fp32_fused_20250722.onnx"));
+                                            "mlpf_5M_attn2x3x256_bm10_elu_checkpoint10_1xa100_fp32_fused_20250809.onnx"));
   desc.add<bool>("use_cuda", false);
   descriptions.addWithDefaultLabel(desc);
 }
