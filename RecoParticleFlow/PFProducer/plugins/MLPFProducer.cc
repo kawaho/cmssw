6fb25f8066525f0a5e246bfff1a6060fa8eee7b2
Joosep Pata <joosep.pata@cern.ch>
2025-08-22 12:29:26 +0300

    switch to mlpf_20M_attn2x3x512_bm12_relu_checkpoint10_8xmi250_fp32_fused_20250818_151118_188371

--- RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
+++ RecoParticleFlow/PFProducer/plugins/MLPFProducer.cc
@@ -268,7 +268,7 @@ void MLPFProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions
   desc.add<edm::InputTag>("src", edm::InputTag("particleFlowBlock"));
   desc.add<edm::FileInPath>("model_path",
                             edm::FileInPath("RecoParticleFlow/PFProducer/data/mlpf/"
-                                            "mlpf_5M_attn2x3x256_bm10_elu_checkpoint10_1xa100_fp32_fused_20250809.onnx"));
+                                            "mlpf_20M_attn2x3x512_bm12_relu_checkpoint10_8xmi250_fp32_fused_20250818_151118_188371.onnx"));
   desc.add<bool>("use_cuda", false);
   descriptions.addWithDefaultLabel(desc);
 }
